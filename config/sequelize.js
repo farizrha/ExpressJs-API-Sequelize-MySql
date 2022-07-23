e6b23cdabcae89b4f038657b2a0b9846040fb5b8
Fariz <farizhaykal1@gmail.com>
2022-07-23 14:17:03 +0700

    first commit

---
+++ config/sequelize.js
@@ -0,0 +1,24 @@
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize({
+    database: 'eduwork-cruds-v2',
+    host: 'localhost',
+    username: 'root',
+    password: '1234',
+    dialect: 'mysql'
+});
+
+// Check connection
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+        } catch (error) {
+        console.error('Unable to connect to the database:', error);
+        }
+})();
+
+module.exports = sequelize;
+
+
+
